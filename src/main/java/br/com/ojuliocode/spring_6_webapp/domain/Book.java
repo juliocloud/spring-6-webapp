0ff93f336163c60c8c893470cb6dabac90e0aa18
juliocloud <juliocesarjuriolli@gmail.com>
2025-08-08 08:17:37 -0300

    chore: getters and setters

--- src/main/java/br/com/ojuliocode/spring_6_webapp/domain/Book.java
+++ src/main/java/br/com/ojuliocode/spring_6_webapp/domain/Book.java
@@ -13,4 +13,28 @@ public class Book {
     public Long id;
     public String title;
     public String isbn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
