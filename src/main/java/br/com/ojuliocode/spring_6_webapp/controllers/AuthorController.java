c18e53e8c102e83b7b10e96fe55312797e3c84d3
juliocloud <juliocesarjuriolli@gmail.com>
2025-08-19 07:58:13 -0300

    fix: removing unused imports

--- src/main/java/br/com/ojuliocode/spring_6_webapp/controllers/AuthorController.java
+++ src/main/java/br/com/ojuliocode/spring_6_webapp/controllers/AuthorController.java
@@ -1,10 +1,8 @@
 package br.com.ojuliocode.spring_6_webapp.controllers;
 
 import br.com.ojuliocode.spring_6_webapp.services.AuthorService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
